84ee6055cd779febfcfd17128901aba9ac63d89d
Trannosaurus <AnT.Tran2003@gmail.com>
2022-12-28 11:34:54 -0800

    temp guide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,18 @@ import CreateRoomForm from "./CreateRoomForm"
 const Home = () => {
         
     return (
+    <>
+        <div> Goto
+            <a href="https://www1.gogoanime.ar/"></a>
+            and choose out an episode to watch, then paste it's link into the 
+            episode url field and start watching!
+            <br/>
+            P.S. use an adblocker
+        </div>
         <div className="w-screen flex items-center justify-center h-screen ">
             <CreateRoomForm />
         </div>
+    </>
     );
 };
 
